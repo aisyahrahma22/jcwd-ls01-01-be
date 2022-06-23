773a857f64d592855779370fa7d5481581ca9db0
Kiki1203 <85237065+Kiki1203@users.noreply.github.com>
2022-06-23 19:30:45 +0700

    Kiki_Register_ProgresFe

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require('express')
-const cors = require('cors')
-const app = express() 
-app.use(express.json()) 
-const PORT = 5000
-app.use(cors())  
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(express.json());
+const PORT = 5000;
+app.use(cors());
 
+const db = require('./Connection/Connection');
 
-const db = require('./Connection/Connection')
+const UserRouters = require('./Routers/UserRouters');
 
-const UserRouters = require('./Routers/UserRouters')
-app.use('/user', UserRouters)
+app.use('/user', UserRouters);
 
-app.use('/Public/', express.static(__dirname + '/Public')) 
+app.use('/Public/', express.static(__dirname + '/Public'));
 
-app.listen(PORT, () => console.log('API Running on PORT ' + PORT) )
+app.listen(PORT, () => console.log('API Running on PORT ' + PORT));
