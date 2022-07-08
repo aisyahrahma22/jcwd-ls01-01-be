d91b36e2412a4aa1f9a5605b341a3c7b500a5fd9
Aisyah Rahmatul Fajrin <98011049+aisyahrahma22@users.noreply.github.com>
2022-07-08 11:23:53 +0700

    aisyahadminedit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 app.use(express.json());
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 
 const db = require('./Connection/Connection');
