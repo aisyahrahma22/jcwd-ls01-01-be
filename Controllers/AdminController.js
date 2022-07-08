fea5dbf49bbeb8bdb85a897e6a4bce537a7e1de2
Kiki1203 <85237065+Kiki1203@users.noreply.github.com>
2022-07-08 11:25:04 +0700

    kiki_daftarproduk

--- Controllers/AdminController.js
+++ Controllers/AdminController.js
@@ -1,122 +1,158 @@
 const db = require('./../Connection/Connection');
-const util = require('util')
-const query = util.promisify(db.query).bind(db)
-const { uploader } = require('../Helpers/Uploader')
+const util = require('util');
+const query = util.promisify(db.query).bind(db);
+const { uploader } = require('../Helpers/Uploader');
 
 // Import Validator
-const validator = require('validator')
+const validator = require('validator');
 
-// Import Crypto 
-const crypto = require('crypto')
+// Import Crypto
+const crypto = require('crypto');
 
 // Import Transporter Nodemailer
-const transporter = require('./../Helpers/Transporter')
+const transporter = require('./../Helpers/Transporter');
 
-const fs = require('fs')
-const handlebars = require('handlebars')
+const fs = require('fs');
+const handlebars = require('handlebars');
 
 // Import JWT Token
-const jwt = require('jsonwebtoken')
-
+const jwt = require('jsonwebtoken');
+const { assign } = require('nodemailer/lib/shared');
 
 module.exports = {
-    login: (req, res) => {
-        try {
-            const data = req.body 
+  login: (req, res) => {
+    try {
+      const data = req.body;
 
-            if(!data.usernameOrEmail || !data.password) throw { message: 'Data incomplete!' }
+      if (!data.usernameOrEmail || !data.password) throw { message: 'Data incomplete!' };
 
-            if(data.usernameOrEmail.includes('@')) {
-            db.query('SELECT * FROM admin WHERE email = ?', [data.usernameOrEmail], (err, result) => {
-                try {
-                    if(err) throw error
+      if (data.usernameOrEmail.includes('@')) {
+        db.query('SELECT * FROM admin WHERE email = ?', [data.usernameOrEmail], (err, result) => {
+          try {
+            if (err) throw error;
 
-                    if(result.length === 1){
-                        if (data.password == result[0].password) {
-                            jwt.sign({id: result[0].id}, '123abc', (err, token) => {
-                                try {
-                                    if(err) throw err
+            if (result.length === 1) {
+              if (data.password == result[0].password) {
+                jwt.sign({ id: result[0].id }, '123abc', (err, token) => {
+                  try {
+                    if (err) throw err;
 
-                                    console.log('ini token with email', token)
-                                    res.status(200).json({
-                                        token: token,
-                                        id: result[0].id
-                                    })
-                                } catch (error) {
-                                    res.status(500).send({
-                                        error: true, 
-                                        message: error.message
-                                    })
-                                }
-                            })
-                        } else {
-                            res.status(400).send({
-                                error: true, 
-                                message: "Incorrect password",
-                            });
-                          }
-                    }else{
-                        res.status(400).send({
-                            error: true, 
-                            message: 'Account not found'
-                        })
-                    }
-                } catch (error) {
+                    console.log('ini token with email', token);
+                    res.status(200).json({
+                      token: token,
+                      id: result[0].id,
+                    });
+                  } catch (error) {
                     res.status(500).send({
-                        error: true, 
-                        message: error.message
-                    })
-                }
-            })
-        } else {
-            db.query('SELECT * FROM admin WHERE username = ?', [data.usernameOrEmail], (err, result) => {
-                try {
-                    if(err) throw error
-                    if(result.length === 1){
-                        if (data.password == result[0].password) {
-                            jwt.sign({id: result[0].id}, '123abc', (err, token) => {
-                                try {
-                                    if(err) throw err
-                                    console.log('ini token with username', token)
-                                    res.status(200).json({
-                                        token: token,
-                                        id: result[0].id
-                                    })
-                                } catch (error) {
-                                    res.status(500).send({
-                                        error: true, 
-                                        message: error.message
-                                    })
-                                }
-                            })
-                        } else {
-                            res.status(400).send({
-                                error: true, 
-                                message: "Incorrect password",
-                            });
-                          }
-                    }else{
-                        res.status(400).send({
-                            error: true, 
-                            message: 'Account not found'
-                        })
-                    }
-                } catch (error) {
-                    res.status(500).send({
-                        error: true, 
-                        message: error.message
-                    })
-                }
-            })
-        }
-        } catch (error) {
+                      error: true,
+                      message: error.message,
+                    });
+                  }
+                });
+              } else {
+                res.status(400).send({
+                  error: true,
+                  message: 'Incorrect password',
+                });
+              }
+            } else {
+              res.status(400).send({
+                error: true,
+                message: 'Account not found',
+              });
+            }
+          } catch (error) {
             res.status(500).send({
-                error: true, 
-                message: error.message
-            })
-        }
-    },
+              error: true,
+              message: error.message,
+            });
+          }
+        });
+      } else {
+        db.query('SELECT * FROM admin WHERE username = ?', [data.usernameOrEmail], (err, result) => {
+          try {
+            if (err) throw error;
+            if (result.length === 1) {
+              if (data.password == result[0].password) {
+                jwt.sign({ id: result[0].id }, '123abc', (err, token) => {
+                  try {
+                    if (err) throw err;
+                    console.log('ini token with username', token);
+                    res.status(200).json({
+                      token: token,
+                      id: result[0].id,
+                    });
+                  } catch (error) {
+                    res.status(500).send({
+                      error: true,
+                      message: error.message,
+                    });
+                  }
+                });
+              } else {
+                res.status(400).send({
+                  error: true,
+                  message: 'Incorrect password',
+                });
+              }
+            } else {
+              res.status(400).send({
+                error: true,
+                message: 'Account not found',
+              });
+            }
+          } catch (error) {
+            res.status(500).send({
+              error: true,
+              message: error.message,
+            });
+          }
+        });
+      }
+    } catch (error) {
+      res.status(500).send({
+        error: true,
+        message: error.message,
+      });
+    }
+  },
 
+  getDataProduct: async (req, res) => {
+    try {
+      const query1 = 'SELECT id, nama_obat AS namaObat,id AS nomorObat, NIE, stok, nilai_barang AS nilaiBarang, harga AS nilaiJual, SatuanObat_id, GolonganObat_id  FROM produk';
+      const products = await query(query1);
 
-    
-}
+      let query2 = `SELECT satuan_obat AS satuanObat FROM satuanobat WHERE id = ?`;
+      for (let i = 0; i < products.length; i++) {
+        let satuan = await query(query2, products[i].SatuanObat_id);
+        products[i] = { ...products[i], satuanObat: satuan[0].satuanObat };
+      }
+      let query3 = `SELECT golongan_obat AS golonganObat FROM golonganobat WHERE id = ?`;
+      for (let i = 0; i < products.length; i++) {
+        let Kategori = await query(query3, products[i].GolonganObat_id);
+        products[i] = { ...products[i], Kategori: Kategori[0].golonganObat };
+      }
+
+      res.status(200).send(products);
+    } catch (error) {
+      console.log(error);
+    }
+  },
+
+  deleteProduct: async (req, res) => {
+    try {
+      const Id = req.params.id;
+
+      const sql1 = 'DELETE FROM produk WHERE id = ?;';
+      let sql1Result = await query(sql1, [Id]);
+
+      res.status(200).send({
+        data: sql1Result,
+        error: false,
+        message: 'Delete Product Success!',
+      });
+    } catch (error) {
+      console.log(error.massage);
+    }
+  },
+};
--- Routers/AdminRouters.js
+++ Routers/AdminRouters.js
@@ -1,13 +1,15 @@
-const { Route } = require('express')
-const express = require('express')
-const Router = express.Router()
+const { Route } = require('express');
+const express = require('express');
+const Router = express.Router();
 
-// Import Controller 
-const AdminController = require('../Controllers/AdminController')
+// Import Controller
+const AdminController = require('../Controllers/AdminController');
 
 // Import JWT Verify
-const jwtVerify = require('../Middleware/JWT')
+const jwtVerify = require('../Middleware/JWT');
 
-Router.post('/loginadmin', AdminController.login) 
+Router.post('/loginadmin', AdminController.login);
+Router.get('/dataproduct', AdminController.getDataProduct);
+Router.delete('/deleteproduct/:id', AdminController.deleteProduct);
 
-module.exports = Router
+module.exports = Router;
