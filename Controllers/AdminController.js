f9d7d1dc14c64e098100c91a9c3ea6bf5aa21102
Aisyah Rahmatul Fajrin <98011049+aisyahrahma22@users.noreply.github.com>
2022-07-10 13:01:27 +0700

    aisyah-diskon

--- Controllers/AdminController.js
+++ Controllers/AdminController.js
@@ -30,17 +30,20 @@ module.exports = {
         db.query('SELECT * FROM admin WHERE email = ?', [data.usernameOrEmail], (err, result) => {
           try {
             if (err) throw error;
-
+            
             if (result.length === 1) {
               if (data.password == result[0].password) {
                 jwt.sign({ id: result[0].id }, '123abc', (err, token) => {
                   try {
                     if (err) throw err;
 
-                    console.log('ini token with email', token);
+                   
                     res.status(200).json({
-                      token: token,
                       id: result[0].id,
+                      username: result[0].username,
+                      email: result[0].email,
+                      token: token,
+                     
                     });
                   } catch (error) {
                     res.status(500).send({
@@ -72,15 +75,20 @@ module.exports = {
         db.query('SELECT * FROM admin WHERE username = ?', [data.usernameOrEmail], (err, result) => {
           try {
             if (err) throw error;
+           
             if (result.length === 1) {
               if (data.password == result[0].password) {
                 jwt.sign({ id: result[0].id }, '123abc', (err, token) => {
                   try {
                     if (err) throw err;
-                    console.log('ini token with username', token);
+                  
                     res.status(200).json({
-                      token: token,
                       id: result[0].id,
+                      username: result[0].username,
+                      email: result[0].email,
+                      token: token,
+                     
+
                     });
                   } catch (error) {
                     res.status(500).send({
@@ -141,172 +149,250 @@ module.exports = {
 
   deleteProduct: async (req, res) => {
     try {
+     
       const Id = req.params.id;
-
+      console.log(Id)
 
       const sql1 = 'DELETE FROM produk WHERE id = ?;';
       let sql1Result = await query(sql1, [Id]);
+      console.log('sql1Result', sql1Result)
+
+      const page = parseInt(req.query.page)
+      const limit = parseInt(req.query.limit)
+      const start = (page - 1) * limit
+      const end = page * limit
+      
+      const sql2 = `Select produk.*, produk.id as nomerObat, golonganobat.golongan_obat, satuanobat.satuan_obat from produk JOIN golonganobat ON produk.GolonganObat_id = golonganobat.id JOIN satuanobat ON produk.SatuanObat_id = satuanobat.id ORDER BY produk.id DESC LIMIT ${start},${limit};`
+      let sql2Result = await query(sql2);
+      console.log('sql2Result', sql2Result)
 
       res.status(200).send({
-        data: sql1Result,
+        deleteData: sql1Result,
         error: false,
         message: 'Delete Product Success!',
+        data: sql2Result
       });
     } catch (error) {
       console.log(error.massage);
     }
   },
-};
 
-    addProduct: (req,res) => {
-        try {
+  addProduct: (req,res) => {
+    try {
+        const path = 'Public/produk/images'; 
+        const upload = uploader(path, 'PRODUK').fields([{ name: 'gambar'}]);
+        const id = req.dataToken.id
+        console.log('ini id', id)
+        upload(req, res, (err) => {
+            if(err){
+                return res.status(500).json({ message: 'Add Image Product failed !', error: err.message });
+            }
+            const { gambar } = req.files;
+            const imagePath = gambar ? path + '/' + gambar[0].filename : null;
+            const data = JSON.parse(req.body.data);
+            data.gambar = imagePath;
+            console.log('data add data', data)
+        
+            
+            var sql = 'INSERT INTO produk SET ?';
+            db.query(sql, data, (err, results) => {
+                console.log('ini results', results)
+                if(err) {
+                    fs.unlinkSync('' + imagePath);
+                    return res.status(500).json({ message: "Server Error", error: err.message });
+                } 
+            
+                sql = `SELECT * from produk`;
+                db.query(sql, (err, results) => {
+                    if(err) {
+                        return res.status(500).json({ message: "Server Error", error: err.message });
+                    }
+                    
+                    return res.status(200).send({ message: "Add Product Success", error: false, results: results });
+                })   
+            })    
+        })
+    } catch(err) {
+        return res.status(500).json({ message: "Server Error", error: err.message });
+    }
+},
+editProduct: (req,res) => {
+    var produk_id = parseInt(req.query.id);
+    var id = req.dataToken.id
+    
+    var sql = `SELECT * from produk where id = ${produk_id};`;
+    db.query(sql, (err, results) => {
+        if(err) throw err;
+        if(results.length > 0) {
             const path = 'Public/produk/images'; 
             const upload = uploader(path, 'PRODUK').fields([{ name: 'gambar'}]);
-            const id = req.dataToken.id
-            console.log('ini id', id)
+
             upload(req, res, (err) => {
                 if(err){
-                    return res.status(500).json({ message: 'Add Image Product failed !', error: err.message });
+                    return res.status(500).json({ message: 'Update Product Image Failed !', error: err.message });
                 }
+
                 const { gambar } = req.files;
                 const imagePath = gambar ? path + '/' + gambar[0].filename : null;
                 const data = JSON.parse(req.body.data);
-                data.gambar = imagePath;
-            
-                
-                var sql = 'INSERT INTO produk SET ?';
-                db.query(sql, data, (err, results) => {
-                    console.log('ini results', results)
-                    if(err) {
-                        fs.unlinkSync('' + imagePath);
-                        return res.status(500).json({ message: "Server Error", error: err.message });
-                    } 
-                
-                    sql = `SELECT * from produk`;
-                    db.query(sql, (err, results) => {
-                        if(err) {
-                            return res.status(500).json({ message: "Server Error", error: err.message });
-                        }
-                        
-                        return res.status(200).send({ message: "Add Product Success", error: false, results: results });
-                    })   
-                })    
-            })
-        } catch(err) {
-            return res.status(500).json({ message: "Server Error", error: err.message });
-        }
-    },
-    editProduct: (req,res) => {
-        var produk_id = parseInt(req.query.id);
-        var id = req.dataToken.id
-        
-        var sql = `SELECT * from produk where id = ${produk_id};`;
-        db.query(sql, (err, results) => {
-            if(err) throw err;
-            if(results.length > 0) {
-                const path = 'Public/produk/images'; 
-                const upload = uploader(path, 'PRODUK').fields([{ name: 'gambar'}]);
-    
-                upload(req, res, (err) => {
-                    if(err){
-                        return res.status(500).json({ message: 'Update Product Image Failed !', error: err.message });
-                    }
-    
-                    const { gambar } = req.files;
-                    const imagePath = gambar ? path + '/' + gambar[0].filename : null;
-                    const data = JSON.parse(req.body.data);
-    
-                    try {
-                        if(imagePath) {
-                            data.gambar = imagePath;   
-                        }
-                        sql = `Update produk set ? where id = ${produk_id};`
-                        db.query(sql,data, (err1,results1) => {
-                            if(err1) {
-                                if(imagePath) {
-                                    fs.unlinkSync('./Public' + imagePath);
-                                }
-                                return res.status(500).json({ message: "Server Error", error: err1.message });
-                            }
 
+                try {
+                    if(imagePath) {
+                        data.gambar = imagePath;   
+                    }
+                    sql = `Update produk set ? where id = ${produk_id};`
+                    db.query(sql,data, (err1,results1) => {
+                        if(err1) {
                             if(imagePath) {
-                                fs.unlinkSync('' + results[0].gambar);
+                                fs.unlinkSync('./Public' + imagePath);
                             }
+                            return res.status(500).json({ message: "Server Error", error: err1.message });
+                        }
 
-                            queryHasil = `SELECT * from produk where id = ${produk_id}`;
-                            db.query(queryHasil, (err4, results4) => {
-                                if(err4) {
-                                    return res.status(500).json({ message: "Server Error", error: err.message });
-                                }
-                                
-                                return res.status(200).send({ message: "Update Product Success", result: results4 });
-                            })   
-                        })
-                    }
-                    catch(err){
-                        console.log(err.message)
-                        return res.status(500).json({ message: "Server Error", error: err.message });
-                    }
-                })
-            }
+                        if(imagePath) {
+                            fs.unlinkSync('' + results[0].gambar);
+                        }
+
+                        queryHasil = `SELECT * from produk where id = ${produk_id}`;
+                        db.query(queryHasil, (err4, results4) => {
+                            if(err4) {
+                                return res.status(500).json({ message: "Server Error", error: err.message });
+                            }
+                            
+                            return res.status(200).send({ message: "Update Product Success", result: results4 });
+                        })   
+                    })
+                }
+                catch(err){
+                    console.log(err.message)
+                    return res.status(500).json({ message: "Server Error", error: err.message });
+                }
+            })
+        }
+    })
+},
+
+getProduct: (req,res) => {
+    const page = parseInt(req.query.page)
+    const limit = parseInt(req.query.limit)
+    const startIndex = (page - 1) * limit
+    const id = req.dataToken.id 
+    var sql = `Select produk.*, produk.id as nomerObat, golonganobat.golongan_obat, satuanobat.satuan_obat from produk JOIN golonganobat ON produk.GolonganObat_id = golonganobat.id JOIN satuanobat ON produk.SatuanObat_id = satuanobat.id ORDER BY produk.id LIMIT ${startIndex},${limit};`
+    db.query(sql, (err,result) => {
+        if(err) return res.status(500).send({ message: 'Error!', error: err})
+        var sql2 = 'SELECT COUNT(produk.id) as TotalData FROM produk;'
+        db.query(sql2, (err2,result2) => {
+            if(err2) return res.status(500).send({ message: 'Error!', error: err2})
+            return res.status(200).json({
+                result1: result2,
+                result2: result
+            })
         })
-    },
-   
-    getProduct: (req,res) => {
+    })
+},
+
+getUnikIDProduct: (req,res) => {
+    var produk_id = parseInt(req.query.id);
+    const id = req.dataToken.id 
+    var sql = `Select produk.*, produk.id as nomerObat, golonganobat.golongan_obat, satuanobat.satuan_obat from produk JOIN golonganobat ON produk.GolonganObat_id = golonganobat.id JOIN satuanobat ON produk.SatuanObat_id = satuanobat.id WHERE produk.id = ${produk_id}`
+    db.query(sql, (err,result) => {
+        if(err) return res.status(500).send({ message: 'Error!', error: err})
+        console.log(result)
+        return res.status(200).json(result)
+    })
+},
+
+pagination: async(req, res) => {
+    try {
         const page = parseInt(req.query.page)
         const limit = parseInt(req.query.limit)
-        const startIndex = (page - 1) * limit
-        const id = req.dataToken.id 
-        var sql = `Select produk.*, produk.id as nomerObat, golonganobat.golongan_obat, satuanobat.satuan_obat from produk JOIN golonganobat ON produk.GolonganObat_id = golonganobat.id JOIN satuanobat ON produk.SatuanObat_id = satuanobat.id ORDER BY produk.id LIMIT ${startIndex},${limit};`
-        db.query(sql, (err,result) => {
-            if(err) return res.status(500).send({ message: 'Error!', error: err})
-            var sql2 = 'SELECT COUNT(produk.id) as TotalData FROM produk;'
-            db.query(sql2, (err2,result2) => {
-                if(err2) return res.status(500).send({ message: 'Error!', error: err2})
-                return res.status(200).json({
-                    result1: result2,
-                    result2: result
-                })
-            })
+        const start = (page - 1) * limit
+        const end = page * limit
+
+        let query1 = `Select produk.*, produk.id as nomerObat, golonganobat.golongan_obat, satuanobat.satuan_obat from produk JOIN golonganobat ON produk.GolonganObat_id = golonganobat.id JOIN satuanobat ON produk.SatuanObat_id = satuanobat.id ORDER BY produk.id LIMIT ${start},${limit};`
+        const findProduk = await query(query1, {
+            attributes: [['id', 'id'], ['nama_obat', 'nama_obat']],
+            order: [['id']],
+            limit: limit,
+            offset: start
         })
-    },
 
-    pagination: async(req, res) => {
-        try {
-            const page = parseInt(req.query.page)
-            const limit = parseInt(req.query.limit)
-            const start = (page - 1) * limit
-            const end = page * limit
+        let query2 = 'SELECT COUNT(produk.id) as TotalData FROM produk;'
+        const countProduk = await query(query2)
+        console.log('countProduk', countProduk[0].TotalData)
 
-            let query1 = `Select produk.*, produk.id as nomerObat, golonganobat.golongan_obat, satuanobat.satuan_obat from produk JOIN golonganobat ON produk.GolonganObat_id = golonganobat.id JOIN satuanobat ON produk.SatuanObat_id = satuanobat.id ORDER BY produk.id LIMIT ${start},${limit};`
-            const findProduk = await query(query1, {
-                attributes: [['id', 'id'], ['nama_obat', 'nama_obat']],
-                order: [['id']],
-                limit: limit,
-                offset: start
-            })
+        let countFiltered = parseInt(countProduk[0].TotalData)
+        let pagination = {}
+        pagination.totalRow = countProduk
+        pagination.totalPage = Math.ceil(countFiltered/limit)
+        console.log('pagination.totalPage', pagination.totalPage)
 
-            let query2 = 'SELECT COUNT(produk.id) as TotalData FROM produk;'
-            const countProduk = await query(query2)
-            console.log('countProduk', countProduk[0].TotalData)
-
-            if(findProduk.length == 0){
-                throw { message: 'Produk Not Found' }
-            }else {
-                res.status(201).send({
-                    status: 201,
-                    error: false,
-                    message: 'Pagination Success',
-                    pagination,
-                    data: findProduk
-                })
+        if (end < countFiltered){
+            pagination.next = {
+                page : pagination.totalPage - page,
+                limit
             }
-        } catch (error) {
-            res.status(500).send({
-                error: true, 
-                message: error.message
+        }
+
+        if (start > 0){
+            pagination.prev = {
+                page : page - 1,
+                limit
+            }
+        }
+
+        if(findProduk.length == 0){
+            throw { message: 'Produk Not Found' }
+        }else {
+            res.status(201).send({
+                status: 201,
+                error: false,
+                message: 'Pagination Success',
+                pagination,
+                data: findProduk
             })
         }
-    }, 
+    } catch (error) {
+        res.status(500).send({
+            error: true, 
+            message: error.message
+        })
+    }
+}, 
+
+search: async(req, res) => {
+  try {
+   
+    const data = parseInt(req.query.kategori)
+    const nama = req.query.nama
+    
+
+   
+     if(data || nama){
+      db.query(`Select produk.*, produk.id as nomerObat, golonganobat.golongan_obat, satuanobat.satuan_obat from produk JOIN golonganobat ON produk.GolonganObat_id = golonganobat.id JOIN satuanobat ON produk.SatuanObat_id = satuanobat.id WHERE produk.nama_obat LIKE "%${nama}%" AND produk.GolonganObat_id = ${data};`, (err, result) => {
+          try {
+              if(err) throw err 
+              
+              res.status(200).send({
+                  error: false, 
+                  result: result
+              })
+          } catch (error) {
+              res.status(500).send({
+                  error: true, 
+                  message: error.message
+              })
+          }
+      })
+     }
+  } catch (error) {
+      res.status(500).send({
+          error: true, 
+          message: error.message
+      })
+  }
+}, 
+
+
 }
 
